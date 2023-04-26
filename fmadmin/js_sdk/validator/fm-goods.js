26bda6446e0ce2e14deaf8ef57beee7268b4eab1
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-04-26 10:55:56 +0800

    商品列表

---
+++ fmadmin/js_sdk/validator/fm-goods.js
@@ -0,0 +1,239 @@
+// 表单校验规则由 schema2code 生成，不建议直接修改校验规则，而建议通过 schema2code 生成, 详情: https://uniapp.dcloud.net.cn/uniCloud/schema
+
+
+const validator = {
+  "image": {
+    "rules": [
+      {
+        "format": "file"
+      }
+    ],
+    "title": "商品主图",
+    "label": "商品主图"
+  },
+  "source_type": {
+    "rules": [
+      {
+        "format": "int"
+      },
+      {
+        "range": [
+          {
+            "text": "库存现货",
+            "value": 0
+          },
+          {
+            "text": "预售采购",
+            "value": 1
+          }
+        ]
+      }
+    ],
+    "title": "商品来源",
+    "defaultValue": 0,
+    "label": "商品来源"
+  },
+  "name": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 10
+      }
+    ],
+    "title": "商品名称",
+    "label": "商品名称"
+  },
+  "producer": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 10
+      }
+    ],
+    "title": "商品产地",
+    "label": "商品产地"
+  },
+  "description": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 200
+      }
+    ],
+    "title": "商品描述",
+    "label": "商品描述"
+  },
+  "unit": {
+    "rules": [
+      {
+        "format": "int"
+      },
+      {
+        "range": [
+          {
+            "text": "斤",
+            "value": 0
+          },
+          {
+            "text": "吨",
+            "value": 1
+          },
+          {
+            "text": "只",
+            "value": 2
+          },
+          {
+            "text": "袋",
+            "value": 3
+          },
+          {
+            "text": "箱",
+            "value": 4
+          }
+        ]
+      }
+    ],
+    "title": "计量单位",
+    "defaultValue": 0,
+    "label": "计量单位"
+  },
+  "price_cost": {
+    "rules": [
+      {
+        "format": "double",
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      },
+      {
+        "minimum": 0,
+        "maximum": 1000000,
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      }
+    ],
+    "title": "成本价",
+    "label": "成本价"
+  },
+  "price_original": {
+    "rules": [
+      {
+        "format": "double",
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      },
+      {
+        "minimum": 0,
+        "maximum": 1000000,
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      }
+    ],
+    "title": "原价",
+    "label": "原价"
+  },
+  "price_sell": {
+    "rules": [
+      {
+        "format": "double",
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      },
+      {
+        "minimum": 0,
+        "maximum": 1000000,
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      }
+    ],
+    "title": "售价",
+    "label": "售价"
+  },
+  "stock": {
+    "rules": [
+      {
+        "format": "int",
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      },
+      {
+        "minimum": 0,
+        "maximum": 1000000,
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      }
+    ],
+    "title": "库存",
+    "label": "库存"
+  },
+  "storage": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 200
+      }
+    ],
+    "title": "存储条件",
+    "label": "存储条件"
+  }
+}
+
+const enumConverter = {
+  "source_type_valuetotext": {
+    "0": "库存现货",
+    "1": "预售采购"
+  },
+  "unit_valuetotext": {
+    "0": "斤",
+    "1": "吨",
+    "2": "只",
+    "3": "袋",
+    "4": "箱"
+  }
+}
+
+function filterToWhere(filter, command) {
+  let where = {}
+  for (let field in filter) {
+    let { type, value } = filter[field]
+    switch (type) {
+      case "search":
+        if (typeof value === 'string' && value.length) {
+          where[field] = new RegExp(value)
+        }
+        break;
+      case "select":
+        if (value.length) {
+          let selectValue = []
+          for (let s of value) {
+            selectValue.push(command.eq(s))
+          }
+          where[field] = command.or(selectValue)
+        }
+        break;
+      case "range":
+        if (value.length) {
+          let gt = value[0]
+          let lt = value[1]
+          where[field] = command.and([command.gte(gt), command.lte(lt)])
+        }
+        break;
+      case "date":
+        if (value.length) {
+          let [s, e] = value
+          let startDate = new Date(s)
+          let endDate = new Date(e)
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+      case "timestamp":
+        if (value.length) {
+          let [startDate, endDate] = value
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+    }
+  }
+  return where
+}
+
+export { validator, enumConverter, filterToWhere }
---
+++ fmadmin/pages/fmgoods/list/edit.vue
@@ -0,0 +1,193 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
+      <uni-forms-item name="image" label="商品主图">
+        <uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="object" v-model="formData.image"></uni-file-picker>
+      </uni-forms-item>
+      <uni-forms-item name="source_type" label="商品来源">
+        <uni-data-checkbox v-model="formData.source_type" :localdata="formOptions.source_type_localdata"></uni-data-checkbox>
+      </uni-forms-item>
+      <uni-forms-item name="name" label="商品名称">
+        <uni-easyinput placeholder="请填写商品名称" v-model="formData.name" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="producer" label="商品产地">
+        <uni-easyinput placeholder="请填写商品产地" v-model="formData.producer" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="description" label="商品描述">
+        <uni-easyinput placeholder="请填写商品描述" v-model="formData.description" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="unit" label="计量单位">
+        <uni-data-checkbox v-model="formData.unit" :localdata="formOptions.unit_localdata"></uni-data-checkbox>
+      </uni-forms-item>
+      <uni-forms-item name="price_cost" label="成本价">
+        <uni-easyinput placeholder="请填写成本价" type="number" v-model="formData.price_cost"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="price_original" label="原价">
+        <uni-easyinput placeholder="请填写原价" type="number" v-model="formData.price_original"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="price_sell" label="售价">
+        <uni-easyinput placeholder="请填写售价" type="number" v-model="formData.price_sell"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="stock" label="库存">
+        <uni-easyinput placeholder="请填写库存" type="number" v-model="formData.stock"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="storage" label="存储条件">
+        <uni-easyinput placeholder="请填写存储条件" v-model="formData.storage" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '@/js_sdk/validator/fm-goods.js';
+
+  const db = uniCloud.database();
+  const dbCmd = db.command;
+  const dbCollectionName = 'fm-goods';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.includes(key)) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "image": null,
+        "source_type": 0,
+        "name": "",
+        "producer": "",
+        "description": "",
+        "unit": 0,
+        "price_cost": null,
+        "price_original": null,
+        "price_sell": null,
+        "stock": null,
+        "storage": ""
+      }
+      return {
+        formData,
+        formOptions: {
+          "source_type_localdata": [
+            {
+              "text": "库存现货",
+              "value": 0
+            },
+            {
+              "text": "预售采购",
+              "value": 1
+            }
+          ],
+          "unit_localdata": [
+            {
+              "text": "斤",
+              "value": 0
+            },
+            {
+              "text": "吨",
+              "value": 1
+            },
+            {
+              "text": "只",
+              "value": 2
+            },
+            {
+              "text": "袋",
+              "value": 3
+            },
+            {
+              "text": "箱",
+              "value": 4
+            }
+          ]
+        },
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onLoad(e) {
+      if (e.id) {
+        const id = e.id
+        this.formDataId = id
+        this.getDetail(id)
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).doc(this.formDataId).update(value).then((res) => {
+          uni.showToast({
+            title: '修改成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      },
+
+      /**
+       * 获取表单数据
+       * @param {Object} id
+       */
+      getDetail(id) {
+        uni.showLoading({
+          mask: true
+        })
+        db.collection(dbCollectionName).doc(id).field("image,source_type,name,producer,description,unit,price_cost,price_original,price_sell,stock,storage").get().then((res) => {
+          const data = res.result.data[0]
+          if (data) {
+            this.formData = data
+            
+          }
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      }
+    }
+  }
+</script>
