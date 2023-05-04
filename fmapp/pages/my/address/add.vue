91df07c36144b407dedc73e7b3721d4a56336e1a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-04 13:17:22 +0800

    Update add.vue

--- fmapp/pages/my/address/add.vue
+++ fmapp/pages/my/address/add.vue
@@ -5,10 +5,10 @@
         <uni-easyinput placeholder="收货人姓名" v-model="formData.receive_name" trim="both"></uni-easyinput>
       </uni-forms-item>
       <uni-forms-item name="receive_mobile" label="手机号码" required>
-        <uni-easyinput placeholder="手机号码" v-model="formData.receive_mobile" trim="both"></uni-easyinput>
+        <uni-easyinput placeholder="手机号码" v-model="formData.receive_mobile" trim="both" maxlength="11"></uni-easyinput>
       </uni-forms-item>
       <uni-forms-item name="area_code" label="所在地区" required>
-        <uni-data-picker self-field="code" parent-field="parent_code" v-model="formData.area_code" collection="opendb-city-china" orderby="value asc" field="code as value, name as text, eq(type, 2) as isleaf"></uni-data-picker>
+        <uni-data-picker self-field="code" parent-field="parent_code" v-model="formData.area_code" collection="opendb-city-china" orderby="value asc" field="code as value, name as text, eq(type, 2) as isleaf" @change="onChangeArea"></uni-data-picker>
       </uni-forms-item>
       <uni-forms-item name="address" label="详细住址" required>
         <uni-easyinput placeholder="省市区后面的详细住址，包含街道小区房间号" v-model="formData.address" trim="both"></uni-easyinput>
@@ -68,7 +68,22 @@
       this.$refs.form.setRules(this.rules)
     },
     methods: {
-      
+      onChangeArea(e) {
+				if (e.detail && e.detail.value && e.detail.value.length === 3) {
+					const value = e.detail.value
+					this.formData.province_name = value[0].text
+					this.formData.province_code = value[0].value
+					this.formData.city_name = value[1].text
+					this.formData.city_code = value[1].value
+					this.formData.area_name = value[2].text
+				} else {
+					this.formData.province_name = ""
+					this.formData.province_code = ""
+					this.formData.city_name = ""
+					this.formData.city_code = ""
+					this.formData.area_name = ""
+				}
+			},
       /**
        * 验证表单并提交
        */
@@ -88,6 +103,12 @@
        * 提交表单
        */
       submitForm(value) {
+				value.province_name = this.formData.province_name
+				value.province_code = this.formData.province_code
+				value.city_name = this.formData.city_name
+				value.city_code = this.formData.city_code
+				value.area_name = this.formData.area_name
+				value.full_address = value.province_name + value.city_name + value.area_name + value.address
         // 使用 clientDB 提交数据
         return db.collection(dbCollectionName).add(value).then((res) => {
           uni.showToast({
