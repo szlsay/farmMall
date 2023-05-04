d14613f6eceafdab3743da5b63faffd61b1719d6
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-04 09:50:28 +0800

    protocol

---
+++ fmadmin/js_sdk/validator/fm-protocol.js
@@ -0,0 +1,129 @@
+// 表单校验规则由 schema2code 生成，不建议直接修改校验规则，而建议通过 schema2code 生成, 详情: https://uniapp.dcloud.net.cn/uniCloud/schema
+
+
+const validator = {
+  "create_time": {
+    "rules": [
+      {
+        "format": "timestamp"
+      }
+    ],
+    "label": "创建时间"
+  },
+  "update_time": {
+    "rules": [
+      {
+        "format": "timestamp"
+      }
+    ],
+    "label": "更新时间"
+  },
+  "title": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 50
+      }
+    ],
+    "label": "协议标题"
+  },
+  "type": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "int"
+      },
+      {
+        "range": [
+          {
+            "text": "用户服务协议",
+            "value": 0
+          },
+          {
+            "text": "用户授权协议",
+            "value": 1
+          },
+          {
+            "text": "隐私政策",
+            "value": 2
+          }
+        ]
+      }
+    ],
+    "label": "协议类型",
+    "defaultValue": 0
+  },
+  "content": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      }
+    ],
+    "label": "协议内容"
+  }
+}
+
+const enumConverter = {
+  "type_valuetotext": {
+    "0": "用户服务协议",
+    "1": "用户授权协议",
+    "2": "隐私政策"
+  }
+}
+
+function filterToWhere(filter, command) {
+  let where = {}
+  for (let field in filter) {
+    let { type, value } = filter[field]
+    switch (type) {
+      case "search":
+        if (typeof value === 'string' && value.length) {
+          where[field] = new RegExp(value)
+        }
+        break;
+      case "select":
+        if (value.length) {
+          let selectValue = []
+          for (let s of value) {
+            selectValue.push(command.eq(s))
+          }
+          where[field] = command.or(selectValue)
+        }
+        break;
+      case "range":
+        if (value.length) {
+          let gt = value[0]
+          let lt = value[1]
+          where[field] = command.and([command.gte(gt), command.lte(lt)])
+        }
+        break;
+      case "date":
+        if (value.length) {
+          let [s, e] = value
+          let startDate = new Date(s)
+          let endDate = new Date(e)
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+      case "timestamp":
+        if (value.length) {
+          let [startDate, endDate] = value
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+    }
+  }
+  return where
+}
+
+export { validator, enumConverter, filterToWhere }
---
+++ fmadmin/pages/fmset/protocol/add.vue
@@ -0,0 +1,120 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
+      <uni-forms-item name="create_time" label="创建时间">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.create_time"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="update_time" label="更新时间">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.update_time"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="title" label="协议标题" required>
+        <uni-easyinput placeholder="请填写协议标题" v-model="formData.title" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="type" label="协议类型" required>
+        <uni-data-checkbox v-model="formData.type" :localdata="formOptions.type_localdata"></uni-data-checkbox>
+      </uni-forms-item>
+      <uni-forms-item name="content" label="协议内容" required>
+        <uni-easyinput placeholder="请填写协议内容" v-model="formData.content"></uni-easyinput>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '@/js_sdk/validator/fm-protocol.js';
+
+  const db = uniCloud.database();
+  const dbCmd = db.command;
+  const dbCollectionName = 'fm-protocol';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.includes(key)) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "create_time": null,
+        "update_time": null,
+        "title": "",
+        "type": 0,
+        "content": ""
+      }
+      return {
+        formData,
+        formOptions: {
+          "type_localdata": [
+            {
+              "text": "用户服务协议",
+              "value": 0
+            },
+            {
+              "text": "用户授权协议",
+              "value": 1
+            },
+            {
+              "text": "隐私政策",
+              "value": 2
+            }
+          ]
+        },
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).add(value).then((res) => {
+          uni.showToast({
+            title: '新增成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      }
+    }
+  }
+</script>
