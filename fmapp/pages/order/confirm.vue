6545d1eaa0a071456bf0d706ed4cfe4357f649fa
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-09 15:59:30 +0800

    Update confirm.vue

--- fmapp/pages/order/confirm.vue
+++ fmapp/pages/order/confirm.vue
@@ -8,11 +8,10 @@
 			</view>
 		</view>
 		<view class="goods">
-			{{cartList}}
+			<!-- 			{{cartList}} -->
 			<view v-for="item in cartList" class="cart-list">
 				<view class="cart-item">
 					<view class="item-left">
-						<radio color="#00CC99" :checked="item.select" @click="onSelectItem(item)" />
 						<image v-if="item.image && item.image.fileType == 'image'" :src="item.image.url" class="item-img"
 							mode="aspectFill"></image>
 					</view>
@@ -29,7 +28,7 @@
 			</view>
 		</view>
 		<view class="footer">
-			
+
 		</view>
 	</view>
 </template>
@@ -43,26 +42,41 @@
 		onMounted,
 		computed
 	} from "vue";
+
+	const fmcart = uniCloud.importObject("fmcart")
 	const cart = useCartStore();
-	const cartList = computed(() => cart.cartList.forEach(item => item.select))
-	// const cartList = computed(() => cart.cartList)
-	
+	const cartList = computed(() => cart.cartList.filter(item => item.select))
+
 	onActivated(() => {
 		cart.getCartList()
 		console.log("onActivated")
 	})
-	
+
 	onMounted(async () => {
 		console.log("onMounted")
 		cart.getCartList()
 	})
+
+	function onChangeNum(data) {
+		const that = this
+		setTimeout(() => {
+			that.updateQty(data)
+		})
+	}
+
+	async function updateQty(data) {
+		await fmcart.updateQty(data._id, data.qty)
+		const result = await fmcart.updateSelect(data._id, data.select)
+		console.log('onSelectItem--', result)
+	}
 </script>
 
 <style lang="scss" scoped>
 	.address {
-		.no-address{
+		.no-address {
 			background-color: white;
 			padding: 32rpx 100rpx 32rpx;
+
 			.no-btn {
 				color: #00CC99;
 				text-align: center;
@@ -75,4 +89,62 @@
 		}
 	}
 
+	.cart-list {
+		display: flex;
+		flex-direction: column;
+
+		.cart-item {
+			display: flex;
+			padding: 32rpx;
+			background-color: white;
+			margin: 32rpx;
+			margin-bottom: 0;
+			border-radius: 16rpx;
+		}
+	}
+
+	.item-left {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.item-select {
+			width: 40rpx;
+			height: 40rpx;
+		}
+
+		.item-img {
+			width: 200rpx;
+			height: 180rpx;
+			border-radius: 16rpx;
+		}
+
+	}
+
+	.item-right {
+		flex: 1;
+		margin-left: 20rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+
+		.title {
+			font-weight: 600;
+			overflow: hidden;
+			-webkit-line-clamp: 2;
+			text-overflow: ellipsis;
+			display: -webkit-box;
+			-webkit-box-orient: vertical;
+		}
+
+		.price {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			text:nth-child(1) {
+				color: #ff442f;
+			}
+		}
+	}
 </style>
