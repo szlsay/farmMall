f90fed551b86345bd8e9aa2d1f47c56fcb4d2ba4
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-17 11:34:49 +0800

    feedback

--- fmadmin/pages/fmuser/feedback/list.vue
+++ fmadmin/pages/fmuser/feedback/list.vue
@@ -8,22 +8,18 @@
       <view class="uni-group">
         <input class="uni-search" type="text" v-model="query" @confirm="search" placeholder="请输入搜索内容" />
         <button class="uni-button" type="default" size="mini" @click="search">搜索</button>
-        <button class="uni-button" type="default" size="mini" @click="navigateTo('./add')">新增</button>
         <button class="uni-button" type="default" size="mini" :disabled="!selectedIndexs.length" @click="delTable">批量删除</button>
-        <download-excel class="hide-on-phone" :fields="exportExcel.fields" :data="exportExcelData" :type="exportExcel.type" :name="exportExcel.filename">
-          <button class="uni-button" type="primary" size="mini">导出 Excel</button>
-        </download-excel>
       </view>
     </view>
     <view class="uni-container">
       <unicloud-db ref="udb" :collection="collectionList" field="uid,create_time,content,image,contact,mobile" :where="where" page-data="replace"
         :orderby="orderby" :getcount="true" :page-size="options.pageSize" :page-current="options.pageCurrent"
-        v-slot:default="{data,pagination,loading,error,options}" :options="options" loadtime="manual" @load="onqueryload">
+        v-slot:default="{data,pagination,loading,error,options}" :options="options" loadtime="manual">
         <uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe type="selection" @selection-change="selectionChange">
           <uni-tr>
             <uni-th align="center" sortable @sort-change="sortChange($event, 'uid')">用户id</uni-th>
             <uni-th align="center" filter-type="timestamp" @filter-change="filterChange($event, 'create_time')" sortable @sort-change="sortChange($event, 'create_time')">创建时间</uni-th>
-            <uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'content')" sortable @sort-change="sortChange($event, 'content')">意见内容</uni-th>
+            <uni-th width="500" align="center" filter-type="search" @filter-change="filterChange($event, 'content')" sortable @sort-change="sortChange($event, 'content')">意见内容</uni-th>
             <uni-th align="center" sortable @sort-change="sortChange($event, 'image')">意见图片</uni-th>
             <uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'contact')" sortable @sort-change="sortChange($event, 'contact')">联系人</uni-th>
             <uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'mobile')" sortable @sort-change="sortChange($event, 'mobile')">联系电话</uni-th>
@@ -43,7 +39,6 @@
             <uni-td align="center">{{item.mobile}}</uni-td>
             <uni-td align="center">
               <view class="uni-group">
-                <button @click="navigateTo('./edit?id='+item._id, false)" class="uni-button" size="mini" type="primary">修改</button>
                 <button @click="confirmDelete(item._id)" class="uni-button" size="mini" type="warn">删除</button>
               </view>
             </uni-td>
@@ -63,7 +58,7 @@
   const db = uniCloud.database()
   // 表查询配置
   const dbOrderBy = '' // 排序字段
-  const dbSearchFields = [] // 模糊搜索字段，支持模糊搜索的字段列表。联表查询格式: 主表字段名.副表字段名，例如用户表关联角色表 role.role_name
+  const dbSearchFields = ['content'] // 模糊搜索字段，支持模糊搜索的字段列表。联表查询格式: 主表字段名.副表字段名，例如用户表关联角色表 role.role_name
   // 分页配置
   const pageSize = 20
   const pageCurrent = 1
@@ -91,20 +86,7 @@
         imageStyles: {
           width: 64,
           height: 64
-        },
-        exportExcel: {
-          "filename": "fm-feedback.xls",
-          "type": "xls",
-          "fields": {
-            "用户id": "uid",
-            "创建时间": "create_time",
-            "content": "content",
-            "意见图片": "image",
-            "contact": "contact",
-            "mobile": "mobile"
-          }
-        },
-        exportExcelData: []
+        }
       }
     },
     onLoad() {
@@ -114,9 +96,6 @@
       this.$refs.udb.loadData()
     },
     methods: {
-      onqueryload(data) {
-        this.exportExcelData = data
-      },
       getWhere() {
         const query = this.query.trim()
         if (!query) {
