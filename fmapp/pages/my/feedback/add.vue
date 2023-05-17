999efe4fdad7bef341529c0089a75ca21f77e5ee
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-17 10:18:36 +0800

    feedback

--- fmapp/pages/my/feedback/add.vue
+++ fmapp/pages/my/feedback/add.vue
@@ -2,22 +2,18 @@
 	<view class="uni-container">
 		<uni-forms ref="form" :value="formData" validate-trigger="submit" err-show-type="toast">
 			<uni-forms-item name="content" label="留言内容" required label-width="160rpx">
-				<!-- 		<textarea @input="binddata('content', $event.detail.value)" class="uni-textarea-border"
-					v-model="formData.content" trim="right"></textarea> -->
 				<uni-easyinput placeholder="请输入留言内容" type="textarea" v-model="formData.content" trim="both"
 					primaryColor="#00CC99" maxlength="200"></uni-easyinput>
 			</uni-forms-item>
 			<uni-forms-item name="imgs" label="意见图片" label-width="160rpx">
-				<uni-file-picker file-mediatype="image" :limit="1" return-type="array" v-model="formData.imgs">
-				</uni-file-picker>
+				<uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="object"
+					v-model="formData.image"></uni-file-picker>
 			</uni-forms-item>
 			<uni-forms-item name="contact" label="联系人" label-width="160rpx">
 				<uni-easyinput placeholder="请输入联系人" v-model="formData.contact" trim="both" maxlength="10"
 					primaryColor="#00CC99"></uni-easyinput>
-				<!-- <uni-easyinput v-model="formData.contact" trim="both"></uni-easyinput> -->
 			</uni-forms-item>
 			<uni-forms-item name="mobile" label="联系电话" label-width="160rpx">
-				<!-- <uni-easyinput v-model="formData.mobile" trim="both"></uni-easyinput> -->
 				<uni-easyinput placeholder="请输入联系电话" v-model="formData.mobile" type="number" trim="both" maxlength="11"
 					primaryColor="#00CC99"></uni-easyinput>
 			</uni-forms-item>
@@ -31,10 +27,10 @@
 <script>
 	import {
 		validator
-	} from './opendb-feedback.js';
+	} from './feedback.js';
 	console.log(validator);
 	const db = uniCloud.database();
-	const dbCollectionName = 'opendb-feedback';
+	const dbCollectionName = 'fm-feedback';
 
 	function getValidator(fields) {
 		let result = {}
@@ -50,7 +46,7 @@
 		data() {
 			let formData = {
 				"content": "",
-				"imgs": [],
+				"image": null,
 				"contact": "",
 				"mobile": ""
 			}
@@ -66,9 +62,6 @@
 			this.$refs.form.setRules(this.rules)
 		},
 		methods: {
-			/**
-			 * 触发表单提交
-			 */
 			submit() {
 				uni.showLoading({
 					mask: true
@@ -81,13 +74,10 @@
 			},
 
 			submitForm(value) {
-				// 使用 clientDB 提交数据
 				db.collection(dbCollectionName).add(value).then((res) => {
 					uni.showToast({
-						icon: 'none',
 						title: '提交成功'
 					})
-					this.getOpenerEventChannel().emit('refreshData')
 					setTimeout(() => uni.navigateBack(), 500)
 				}).catch((err) => {
 					uni.showModal({
@@ -107,7 +97,7 @@
 		padding: 10rpx 32rpx 10rpx;
 		margin-top: 100rpx;
 		background-color: white;
-	
+
 		.btn-add {
 			width: 100%;
 			height: 80rpx;
@@ -118,13 +108,12 @@
 			line-height: 80rpx;
 		}
 	}
-	
+
 	.uni-container {
 		padding: 32rpx;
 	}
-	
+
 	page {
 		background-color: white;
 	}
-
 </style>
