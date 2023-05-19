107c24a325f120569f680f807a5bc955a32012c8
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-19 09:22:19 +0800

    map

--- fmadmin/pages/fmset/ro/add.vue
+++ fmadmin/pages/fmset/ro/add.vue
@@ -1,124 +1,217 @@
 <template>
-  <view class="uni-container">
-    <uni-forms ref="form" :model="formData" validateTrigger="bind">
-      <uni-forms-item name="name" label="合作社名称">
-        <uni-easyinput placeholder="请填写合作社名称" v-model="formData.name" trim="both"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="mobile" label="手机号码">
-        <uni-easyinput placeholder="手机号码" v-model="formData.mobile" trim="both"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="start" label="星级">
-        <uni-data-select placeholder="请选择星级" v-model="formData.start" :localdata="formOptions.start_localdata"></uni-data-select>
-      </uni-forms-item>
-      <view class="uni-button-group">
-        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
-        <navigator open-type="navigateBack" style="margin-left: 15px;">
-          <button class="uni-button" style="width: 100px;">返回</button>
-        </navigator>
-      </view>
-    </uni-forms>
-  </view>
+	<view class="uni-container">
+		<uni-forms ref="form" :model="formData" validateTrigger="bind">
+			<uni-forms-item name="name" label="合作社名称">
+				<uni-easyinput placeholder="请填写合作社名称" v-model="formData.name" trim="both"></uni-easyinput>
+			</uni-forms-item>
+			<uni-forms-item name="mobile" label="手机号码">
+				<uni-easyinput placeholder="手机号码" v-model="formData.mobile" trim="both"></uni-easyinput>
+			</uni-forms-item>
+			<uni-forms-item name="start" label="星级">
+				<uni-data-select placeholder="请选择星级" v-model="formData.start"
+					:localdata="formOptions.start_localdata"></uni-data-select>
+			</uni-forms-item>
+			<view id="map">
+
+			</view>
+			<view class="uni-button-group">
+				<button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+				<navigator open-type="navigateBack" style="margin-left: 15px;">
+					<button class="uni-button" style="width: 100px;">返回</button>
+				</navigator>
+			</view>
+		</uni-forms>
+	</view>
 </template>
 
 <script>
-  import { validator } from '@/js_sdk/validator/fm-ro.js';
+	import {
+		validator
+	} from '@/js_sdk/validator/fm-ro.js';
+	//引入 amap-jsapi-loader
+	import AMapLoader from "@amap/amap-jsapi-loader";
+	//安全密钥引入
+	window._AMapSecurityConfig = {
+		securityJsCode: "6a80c5d3ba9586cdb3434946a291aecf",
+	};
+	const db = uniCloud.database();
+	const dbCmd = db.command;
+	const dbCollectionName = 'fm-ro';
 
-  const db = uniCloud.database();
-  const dbCmd = db.command;
-  const dbCollectionName = 'fm-ro';
+	function getValidator(fields) {
+		let result = {}
+		for (let key in validator) {
+			if (fields.includes(key)) {
+				result[key] = validator[key]
+			}
+		}
+		return result
+	}
 
-  function getValidator(fields) {
-    let result = {}
-    for (let key in validator) {
-      if (fields.includes(key)) {
-        result[key] = validator[key]
-      }
-    }
-    return result
-  }
 
-  
 
-  export default {
-    data() {
-      let formData = {
-        "name": "",
-        "mobile": "",
-        "start": 0
-      }
-      return {
-        formData,
-        formOptions: {
-          "start_localdata": [
-            {
-              "text": "无",
-              "value": 0
-            },
-            {
-              "text": "一星",
-              "value": 1
-            },
-            {
-              "text": "二星",
-              "value": 2
-            },
-            {
-              "text": "三星",
-              "value": 3
-            },
-            {
-              "text": "四星",
-              "value": 4
-            },
-            {
-              "text": "五星",
-              "value": 5
-            }
-          ]
-        },
-        rules: {
-          ...getValidator(Object.keys(formData))
-        }
-      }
-    },
-    onReady() {
-      this.$refs.form.setRules(this.rules)
-    },
-    methods: {
-      
-      /**
-       * 验证表单并提交
-       */
-      submit() {
-        uni.showLoading({
-          mask: true
-        })
-        this.$refs.form.validate().then((res) => {
-          return this.submitForm(res)
-        }).catch(() => {
-        }).finally(() => {
-          uni.hideLoading()
-        })
-      },
+	export default {
+		data() {
+			let formData = {
+				"name": "",
+				"mobile": "",
+				"start": 0
+			}
+			return {
+				map: null, //高德实例
+				marker: null, //点标记 Marker
+				geocoder: null, //逆向地理
+				formData,
+				formOptions: {
+					"start_localdata": [{
+							"text": "无",
+							"value": 0
+						},
+						{
+							"text": "一星",
+							"value": 1
+						},
+						{
+							"text": "二星",
+							"value": 2
+						},
+						{
+							"text": "三星",
+							"value": 3
+						},
+						{
+							"text": "四星",
+							"value": 4
+						},
+						{
+							"text": "五星",
+							"value": 5
+						}
+					]
+				},
+				rules: {
+					...getValidator(Object.keys(formData))
+				}
+			}
+		},
+		onLoad() {
+			this.initMap();
+		},
+		onReady() {
+			this.$refs.form.setRules(this.rules)
+		},
+		methods: {
+			//初始化
+			initMap() {
+				AMapLoader.load({
+						key: "902207ba23e27ca1ead75ebca4694010", // 申请好的Web端开发者Key，首次调用 load 时必填
+						version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+						plugins: ["AMap.CitySearch", "AMap.Geocoder"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+					}).then((AMap) => {
+						// 实例化
+						this.map = new AMap.Map("map", { //设置地图容器id
+							viewMode: "3D", //是否为3D地图模式
+							zoom: 16, //初始化地图级别
+							center: [106.583541, 29.563475], //初始化地图中心点位置
+						});
 
-      /**
-       * 提交表单
-       */
-      submitForm(value) {
-        // 使用 clientDB 提交数据
-        return db.collection(dbCollectionName).add(value).then((res) => {
-          uni.showToast({
-            title: '新增成功'
-          })
-          this.getOpenerEventChannel().emit('refreshData')
-          setTimeout(() => uni.navigateBack(), 500)
-        }).catch((err) => {
-          uni.showModal({
-            content: err.message || '请求服务失败',
-            showCancel: false
-          })
-        })
-      }
-    }
-  }
+						// 自动获取用户IP，返回当前城市
+						let citysearch = new AMap.CitySearch();
+						citysearch.getLocalCity((status, result) => {
+							if (status === "complete" && result.info === "OK") {
+								console.log(result);
+							}
+						});
+
+						// 地图点击事件--点标记标注
+						this.map.on("click", this.handleClick);
+
+						// 逆向地理编码插件
+						this.geocoder = new AMap.Geocoder({
+							// city: "010", //城市设为北京，默认：“全国”
+							radius: 1000, //范围，默认：500
+						});
+					})
+					.catch((e) => {
+						console.log(e);
+					});
+			},
+			// 地图点击之后更新点标记
+			handleClick(e) {
+				let longitude = e.lnglat.getLng(); //经度
+				let latitude = e.lnglat.getLat(); //纬度
+				// 逆向地理编码
+				this.geocoder.getAddress([longitude, latitude], (status, result) => {
+					if (status === "complete" && result.info == "OK") {
+						let address = result.regeocode.formattedAddress;
+						// 更新点标记
+						this.updateMap(address, [longitude, latitude]);
+					} else {
+						console.log("定位失败，请稍后重试");
+					}
+				});
+			},
+			// 更新点标记
+			updateMap(address, lnglat) {
+				// 移除已创建的 marker
+				if (this.marker) this.map.remove(this.marker);
+				// 同时设置地图中心点和缩放级别
+				this.map.setZoomAndCenter(16, lnglat);
+				// 自定义标记点
+				this.marker = new AMap.Marker({
+					position: lnglat,
+					icon: "http://vdata.amap.com/icons/b18/1/2.png",
+					anchor: "top-center",
+					offset: new AMap.Pixel(-10, -10),
+				});
+				// 添加到实例
+				this.marker.setMap(this.map);
+				// 设置label标签，label默认蓝框白底左上角显示，样式className为：amap-marker-label
+				this.marker.setLabel({
+					direction: "top-center",
+					offset: new AMap.Pixel(10, 0), //设置文本标注偏移量
+					content: "<div style='width:250px;'>" + address + "</div>", //设置文本标注内容
+				});
+			},
+			/**
+			 * 验证表单并提交
+			 */
+			submit() {
+				uni.showLoading({
+					mask: true
+				})
+				this.$refs.form.validate().then((res) => {
+					return this.submitForm(res)
+				}).catch(() => {}).finally(() => {
+					uni.hideLoading()
+				})
+			},
+
+			/**
+			 * 提交表单
+			 */
+			submitForm(value) {
+				// 使用 clientDB 提交数据
+				return db.collection(dbCollectionName).add(value).then((res) => {
+					uni.showToast({
+						title: '新增成功'
+					})
+					this.getOpenerEventChannel().emit('refreshData')
+					setTimeout(() => uni.navigateBack(), 500)
+				}).catch((err) => {
+					uni.showModal({
+						content: err.message || '请求服务失败',
+						showCancel: false
+					})
+				})
+			}
+		}
+	}
 </script>
+
+<style lang="scss" scoped>
+#map {
+  width: 600px;
+  height: 300px;
+}
+</style>
