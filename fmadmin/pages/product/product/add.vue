740c5ccc80c91219a56ab60f38963fb9f1d17f4d
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-05 21:25:45 +0800

    Update add.vue

--- fmadmin/pages/product/product/add.vue
+++ fmadmin/pages/product/product/add.vue
@@ -1,26 +1,45 @@
 <template>
 	<view class="uni-container">
 		<uni-forms ref="form" :model="formData" validateTrigger="bind">
-			<uni-forms-item name="name" label="产品名称">
-				<uni-easyinput placeholder="请填写产品名称" v-model="formData.name" trim="both"></uni-easyinput>
-			</uni-forms-item>
-			<uni-forms-item name="raw_name" label="原材料名称">
-				<uni-easyinput placeholder="请填写原材料名称" v-model="formData.raw_name" trim="both"></uni-easyinput>
-			</uni-forms-item>
-			<uni-forms-item name="unit" label="计量单位">
-				<uni-data-select placeholder="请填写计量单位" v-model="formData.unit"></uni-data-select>
-			</uni-forms-item>
-			<uni-forms-item name="unit_title" label="计量单位">
-				<uni-easyinput placeholder="请填写计量单位" v-model="formData.unit_title"></uni-easyinput>
-			</uni-forms-item>
-			<uni-forms-item name="image" label="产品主图">
-				<uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="object"
-					v-model="formData.image"></uni-file-picker>
-			</uni-forms-item>
-			<uni-forms-item name="image_content" label="展示图片">
-				<uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="array"
-					v-model="formData.image_content"></uni-file-picker>
-			</uni-forms-item>
+
+			<view class="fm-box">
+				<view class="fm-card-header">基本信息</view>
+				<uni-row>
+					<uni-col :xs="24" :sm="8">
+						<uni-forms-item required name="name" label="产品名称" :label-width="labelWidth" label-align="right">
+							<uni-easyinput placeholder="请填写产品名称" v-model="formData.name" trim="both"></uni-easyinput>
+						</uni-forms-item>
+					</uni-col>
+					<uni-col :xs="24" :sm="8">
+						<uni-forms-item name="raw_name" label="原料名称" :label-width="labelWidth" label-align="right">
+							<uni-easyinput placeholder="请填写原材料名称" v-model="formData.raw_name"
+								trim="both"></uni-easyinput>
+						</uni-forms-item>
+					</uni-col>
+					<uni-col :xs="24" :sm="8">
+						<uni-forms-item name="unit" label="计量单位" :label-width="labelWidth" label-align="right">
+							<uni-data-select placeholder="请选择计量单位" v-model="formData.unit"
+								:localdata="$store.state.sys.measure_units" @change="onChangeUnit"
+								ref="dataSelectUnit"></uni-data-select>
+						</uni-forms-item>
+					</uni-col>
+				</uni-row>
+				<uni-row>
+					<uni-forms-item name="image" label="套餐主图" :label-width="labelWidth" label-align="right">
+						<uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="object"
+							v-model="formData.image" :image-styles="imageStyles"></uni-file-picker>
+						<text style="color: red; font-size: 14px;">用于套餐的封面展示。格式：jpg,png,webp，建议400*400px的图片</text>
+					</uni-forms-item>
+				</uni-row>
+				<uni-row>
+					<uni-forms-item name="image_content" label="展示图片" :label-width="labelWidth" label-align="right">
+						<uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="array"
+							v-model="formData.image_content" limit="6" :image-styles="imageStyles"></uni-file-picker>
+						<text style="color: red; font-size: 14px;"
+							class="title-alert">用于套餐的详情展示，最多六张。格式：jpg,png,webp</text>
+					</uni-forms-item>
+				</uni-row>
+			</view>
 			<view class="uni-button-group">
 				<button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
 				<navigator open-type="navigateBack" style="margin-left: 15px;">
@@ -63,6 +82,11 @@
 				"image_content": []
 			}
 			return {
+				imageStyles: {
+					width: 140,
+					height: 140,
+				},
+				labelWidth: 80,
 				formData,
 				formOptions: {},
 				rules: {
