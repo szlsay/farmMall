f31b0316dbda32507e02f1db094b2b515932e83a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-10 01:01:25 +0800

    combo

--- fmadmin/js_sdk/validator/fm-combo.js
+++ fmadmin/js_sdk/validator/fm-combo.js
@@ -2,169 +2,165 @@
 
 
 const validator = {
-	"name": {
-		"rules": [{
-				"required": true
-			},
-			{
-				"format": "string"
-			},
-			{
-				"maxLength": 10
-			}
-		],
-		"label": "套餐名称"
-	},
-	"unit": {
-		"rules": [{
-				"required": true
-			},
-			{
-				"format": "string"
-			}
-		],
-		"label": "计量单位"
-	},
-	"image": {
-		"rules": [{
-			"format": "file"
-		}],
-		"label": "套餐主图"
-	},
-	"image_content": {
-		"rules": [{
-				"format": "array"
-			},
-			{
-				"arrayType": "file"
-			}
-		],
-		"label": "展示图片"
-	},
-	"sku": {
-		"rules": [{
-				"format": "array"
-			},
-			{
-				"arrayType": "object"
-			}
-		],
-		"label": "套餐规格"
-	},
-	"delivery_rate": {
-		"rules": [{
-			"format": "string"
-		}],
-		"label": "配送频率"
-	},
-	"delivery_rate_title": {
-		"rules": [{
-			"format": "string"
-		}],
-		"label": "配送频率"
-	},
-	"price_sell": {
-		"rules": [{
-				"format": "double",
-				"errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
-			},
-			{
-				"minimum": 0,
-				"maximum": 1000000,
-				"errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
-			}
-		],
-		"label": "售价"
-	},
-	"expiry": {
-		"rules": [{
-				"format": "int",
-				"errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
-			},
-			{
-				"minimum": 0,
-				"maximum": 1000000,
-				"errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
-			}
-		],
-		"label": "保质期"
-	},
-	"reserve_begin": {
-		"rules": [{
-			"format": "timestamp"
-		}],
-		"label": "预订开始时间"
-	},
-	"reserve_end": {
-		"rules": [{
-			"format": "timestamp"
-		}],
-		"label": "预订结束时间"
-	},
-	"description": {
-		"rules": [{
-				"format": "string"
-			},
-			{
-				"maxLength": 200
-			}
-		],
-		"label": "产品描述"
-	}
+  "name": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 10
+      }
+    ],
+    "label": "套餐名称"
+  },
+  "unit": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      }
+    ],
+    "label": "产品单位"
+  },
+  "image": {
+    "rules": [
+      {
+        "format": "file"
+      }
+    ],
+    "label": "套餐主图"
+  },
+  "image_content": {
+    "rules": [
+      {
+        "format": "array"
+      },
+      {
+        "arrayType": "file"
+      }
+    ],
+    "label": "展示图片"
+  },
+  "sku": {
+    "rules": [
+      {
+        "format": "array"
+      },
+      {
+        "arrayType": "object"
+      }
+    ],
+    "label": "套餐规格"
+  },
+  "sell_price": {
+    "rules": [
+      {
+        "format": "double",
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      },
+      {
+        "minimum": 0,
+        "maximum": 1000000,
+        "errorMessage": "{label}应该大于 {minimum} ，小于 {maximum} "
+      }
+    ],
+    "label": "售价"
+  },
+  "delivery_rate": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "label": "配送频率"
+  },
+  "delivery_timer": {
+    "rules": [
+      {
+        "format": "number"
+      }
+    ],
+    "label": "配送次数"
+  },
+  "reserve_begin": {
+    "rules": [
+      {
+        "format": "timestamp"
+      }
+    ],
+    "label": "预订开始时间"
+  },
+  "reserve_end": {
+    "rules": [
+      {
+        "format": "timestamp"
+      }
+    ],
+    "label": "预订结束时间"
+  },
+  "description": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 200
+      }
+    ],
+    "label": "产品描述"
+  }
 }
 
 const enumConverter = {}
 
 function filterToWhere(filter, command) {
-	let where = {}
-	for (let field in filter) {
-		let {
-			type,
-			value
-		} = filter[field]
-		switch (type) {
-			case "search":
-				if (typeof value === 'string' && value.length) {
-					where[field] = new RegExp(value)
-				}
-				break;
-			case "select":
-				if (value.length) {
-					let selectValue = []
-					for (let s of value) {
-						selectValue.push(command.eq(s))
-					}
-					where[field] = command.or(selectValue)
-				}
-				break;
-			case "range":
-				if (value.length) {
-					let gt = value[0]
-					let lt = value[1]
-					where[field] = command.and([command.gte(gt), command.lte(lt)])
-				}
-				break;
-			case "date":
-				if (value.length) {
-					let [s, e] = value
-					let startDate = new Date(s)
-					let endDate = new Date(e)
-					where[field] = command.and([command.gte(startDate), command.lte(endDate)])
-				}
-				break;
-			case "timestamp":
-				if (value.length) {
-					let [startDate, endDate] = value
-					where[field] = command.and([command.gte(startDate), command.lte(endDate)])
-				}
-				break;
-		}
-	}
-	return where
+  let where = {}
+  for (let field in filter) {
+    let { type, value } = filter[field]
+    switch (type) {
+      case "search":
+        if (typeof value === 'string' && value.length) {
+          where[field] = new RegExp(value)
+        }
+        break;
+      case "select":
+        if (value.length) {
+          let selectValue = []
+          for (let s of value) {
+            selectValue.push(command.eq(s))
+          }
+          where[field] = command.or(selectValue)
+        }
+        break;
+      case "range":
+        if (value.length) {
+          let gt = value[0]
+          let lt = value[1]
+          where[field] = command.and([command.gte(gt), command.lte(lt)])
+        }
+        break;
+      case "date":
+        if (value.length) {
+          let [s, e] = value
+          let startDate = new Date(s)
+          let endDate = new Date(e)
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+      case "timestamp":
+        if (value.length) {
+          let [startDate, endDate] = value
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+    }
+  }
+  return where
 }
 
-export {
-	validator,
-	enumConverter,
-	filterToWhere
-}
+export { validator, enumConverter, filterToWhere }
--- fmadmin/pages/product/combo/add.vue
+++ fmadmin/pages/product/combo/add.vue
@@ -36,18 +36,18 @@
 				<view class="fm-card-header">套餐规格 (最大数量为{{skuMax}}个商品)</view>
 				<uni-row v-for="(item, index) in formData.sku" :key="index">
 					<uni-col :xs="24" :sm="6">
-						<uni-forms-item required :name="['sku',index,'goods_id']" label="商品名称" :label-width="labelWidth"
-							label-align="right">
-							<uni-data-select collection="fm-goods" field="_id as value, name as text"
-								v-model="item.goods_id" @change="onChangeGoods(index)" ref="dataSelectGoods"
-								placeholder="请选择商品" :key="'goods' + index" />
+						<uni-forms-item required :name="['sku',index,'product_id']" label="商品名称"
+							:label-width="labelWidth" label-align="right">
+							<uni-data-select :localdata="productList" v-model="item.product_id"
+								@change="onChangeProduct(index)" ref="dataSelectProduct" placeholder="请选择商品"
+								:key="'product' + index" />
 						</uni-forms-item>
 					</uni-col>
 					<uni-col :xs="24" :sm="6">
-						<uni-forms-item required :name="['sku',index,'unit']" label="计量单位" :label-width="labelWidth"
+						<uni-forms-item :name="['sku',index,'unit']" label="计量单位" :label-width="labelWidth"
 							label-align="right">
-							<uni-data-select placeholder="请选择计量单位" v-model="item.unit"
-								:localdata="$store.state.sys.measure_units"></uni-data-select>
+							<uni-data-select placeholder="自动匹配计量单位" v-model="item.unit"
+								:localdata="$store.state.sys.measure_units" disabled></uni-data-select>
 						</uni-forms-item>
 					</uni-col>
 					<uni-col :xs="24" :sm="6">
@@ -84,11 +84,6 @@
 						<uni-forms-item name="price_sell" label="售价" :label-width="labelWidth" label-align="right">
 							<uni-easyinput placeholder="请填写售价" type="number"
 								v-model="formData.price_sell"></uni-easyinput>
-						</uni-forms-item>
-					</uni-col>
-					<uni-col :xs="24" :sm="12">
-						<uni-forms-item name="expiry" label="保质期(天)" :label-width="labelWidth" label-align="right">
-							<uni-easyinput placeholder="请填写保质期" type="number" v-model="formData.expiry"></uni-easyinput>
 						</uni-forms-item>
 					</uni-col>
 				</uni-row>
@@ -146,10 +141,9 @@
 				"image": null,
 				"image_content": [],
 				"sku": [],
+				"sell_price": null,
 				"delivery_rate": "",
-				"delivery_rate_title": "",
-				"price_sell": null,
-				"expiry": null,
+				"delivery_timer": null,
 				"reserve_begin": null,
 				"reserve_end": null,
 				"description": ""
@@ -164,28 +158,55 @@
 				formData,
 				rules: {
 					...getValidator(Object.keys(formData))
-				}
+				},
+				productList: []
 			}
 		},
 		onReady() {
 			this.$refs.form.setRules(this.rules)
 			this.onAddSku()
+			this.getProducts()
 		},
 		methods: {
+			getProducts() {
+				const db = uniCloud.database()
+				db.collection("fm-product").where({
+					is_delete: false
+				}).field("_id, name, unit, market_price").get().then(res => {
+
+					const result = res.result.data.map(item => {
+						item.value = item._id
+						item.text = item.name
+						return item
+					})
+					this.productList = result
+				})
+			},
 			onChangeDelivery() {
 				setTimeout(() => {
 					this.formData.delivery_rate_title = this.$refs.dataSelectDelivery.current
 				}, 100)
 			},
-			onChangeGoods(index) {
+			onChangeProduct(index) {
 				setTimeout(() => {
-					this.formData.sku[index].goods_name = this.$refs.dataSelectGoods[index].current
+					const name = this.$refs.dataSelectProduct[index].current
+					if (name && name != '') {
+						const product = this.productList.filter(item => item.name === name)[0]
+						this.formData.sku[index].unit = product.unit
+						this.formData.sku[index].product_name = product.name
+						this.formData.sku[index].market_price = product.market_price
+					} else {
+						this.formData.sku[index].product_id = null
+						this.formData.sku[index].unit = null
+						this.formData.sku[index].product_name = null
+						this.formData.sku[index].market_price = null
+					}
 				}, 100)
 			},
 			onAddSku() {
 				const sku = {
-					"goods_id": null,
-					"goods_name": null,
+					"product_id": null,
+					"product_name": null,
 					"qty": null,
 					"unit": null,
 				}
@@ -194,7 +215,6 @@
 			onDeleteSku(index) {
 				this.formData.sku.splice(index, 1)
 			},
-
 			submit() {
 				uni.showLoading({
 					mask: true
@@ -208,7 +228,6 @@
 			submitForm(value) {
 				if (this.formData.sku.length) value.sku = this.formData.sku
 				value.delivery_rate_title = this.formData.delivery_rate_title
-
 				const fmcombo = uniCloud.importObject("fm-combo")
 				fmcombo.add(value).then((res) => {
 					uni.showToast({
