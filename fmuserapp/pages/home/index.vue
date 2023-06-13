9840ab6c0c898781e601dfeabb5574579c2613d5
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-14 05:25:51 +0800

    pages

--- fmuserapp/pages/home/index.vue
+++ fmuserapp/pages/home/index.vue
@@ -1,22 +1,167 @@
 <template>
-	<view>
-		首页
+	<view class="container">
+		<view class="top">
+			<view class="menu-contant">
+				<view class="item-menu" v-for="(item, index) in menus" :key="index">
+					<i class="iconfont" :class="item.icon"></i>
+					<text>{{item.title}}</text>
+				</view>
+			</view>
+			<view class="search-contant">
+				<button class="btn-search">
+					<i class="iconfont fm-search"></i>
+					<text>买好货 上农福</text>
+				</button>
+			</view>
+		</view>
+		<unicloud-db ref="udbBanner" v-slot:default="{data, pagination, loading, hasMore, error}"
+			:collection="collectionListBanner" field="image,open_url,title,sort,status,description"
+			where="status == true">
+			<view class="banner">
+				<swiper circular indicator-dots>
+					<swiper-item v-for="(item, index) in data" :key="index">
+						<image v-if="item.image && item.image.fileType == 'image'" :src="item.image.url"
+							class="fm_image" mode="aspectFill"></image>
+					</swiper-item>
+				</swiper>
+			</view>
+		</unicloud-db>
+		<unicloud-db ref="udb" v-slot:default="{data, pagination, loading, hasMore, error}" :collection="collectionList"
+			field="image,name,description">
+			<view v-if="error">{{error.message}}</view>
+			<view v-else-if="data" class="goods">
+				{{data}}
+			</view>
+			<uni-load-more :status="loading?'loading':(hasMore ? 'more' : 'noMore')"></uni-load-more>
+		</unicloud-db>
 	</view>
 </template>
 
 <script>
+	import {
+		cloneObject
+	} from "@/utils"
+	const db = uniCloud.database()
 	export default {
 		data() {
 			return {
-
+				collectionListBanner: "fm-banner",
+				collectionList: "fm-goods",
+				loadMore: {
+					contentdown: '',
+					contentrefresh: '',
+					contentnomore: ''
+				},
+				menus: [{
+						"title": "农货批发",
+						"icon": "fm-jinpaigongyings"
+					},
+					{
+						"title": "产地资源",
+						"icon": "fm-suyuan"
+					},
+					{
+						"title": "买家保障",
+						"icon": "fm-maijiabz"
+					}
+				]
 			}
 		},
+		onPullDownRefresh() {
+			this.$refs.udb.loadData({
+				clear: true
+			}, () => {
+				uni.stopPullDownRefresh()
+			})
+		},
+		onReachBottom() {
+			this.$refs.udb.loadMore()
+		},
 		methods: {
-
+			handleItemClick(item) {
+				console.log("handleItemClick---", item._id)
+				uni.navigateTo({
+					url: './detail?id=' + item._id
+				})
+			},
+			changeShowData(data) {
+				const copyData = cloneObject(data)
+				return copyData.map(item => {
+					if (item.image && item.image.url) {
+						item.image = item.image.url
+					}
+					item.desc = item.description
+					item.title = item.name
+					return item
+				})
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	@import "@/static/css/iconfont.css";
 
+	.goods {
+		padding: 16rpx 32rpx 16rpx;
+	}
+
+	.banner {
+		margin-top: -2rpx;
+		padding: 16rpx 32rpx 16rpx;
+		height: 300rpx;
+		background: linear-gradient(to bottom, #36A54B 50%, #86f59B 100%);
+
+		uni-swiper {
+			height: 100%;
+
+			.fm_image {
+				width: 100%;
+				height: 100%;
+				border-radius: 16rpx;
+			}
+		}
+	}
+
+	.top {
+		background-color: #36A54B;
+
+		.menu-contant {
+			display: flex;
+			justify-content: space-evenly;
+			color: white;
+			padding-top: 32rpx;
+			padding-bottom: 20rpx;
+
+			.item-menu {
+				display: flex;
+				align-items: center;
+
+				text {
+					color: white;
+					margin-left: 8rpx;
+				}
+			}
+		}
+
+		.search-contant {
+			padding-left: 32rpx;
+			padding-right: 32rpx;
+			padding-bottom: 20rpx;
+
+			.btn-search {
+				width: 100%;
+				height: 80rpx;
+				background-color: white;
+				border-radius: 40rpx;
+				color: #333333;
+				text-align: center;
+				line-height: 80rpx;
+
+				text {
+					margin-left: 8rpx;
+				}
+			}
+		}
+	}
 </style>
---
+++ fmuserapp/utils/index.js
@@ -0,0 +1,75 @@
+/**
+ * 是否登录
+ */
+export function isLogin() {
+	const value = uni.getStorageSync('uni_id_token')
+	if (value && value != '') {
+		return true
+	} else {
+		return false
+	}
+}
+/**
+ * 深度克隆对象、数组
+ * @param obj 被克隆的对象
+ * @return 克隆后的对象
+ */
+export function cloneObject(obj) {
+	return JSON.parse(JSON.stringify(obj))
+}
+
+/**
+ * 时间格式化
+ * @param value
+ * @param fmt
+ * @returns {*}
+ */
+export function formatDate(value, fmt = 'yyyy/MM/dd hh:mm:ss') {
+	let regPos = /^\d+(\.\d+)?$/;
+	if (regPos.test(value)) {
+		//如果是数字
+		let getDate = new Date(value);
+		let o = {
+			'M+': getDate.getMonth() + 1,
+			'd+': getDate.getDate(),
+			'h+': getDate.getHours(),
+			'm+': getDate.getMinutes(),
+			's+': getDate.getSeconds(),
+			'q+': Math.floor((getDate.getMonth() + 3) / 3),
+			'S': getDate.getMilliseconds()
+		};
+		if (/(y+)/.test(fmt)) {
+			fmt = fmt.replace(RegExp.$1, (getDate.getFullYear() + '').substr(4 - RegExp.$1.length))
+		}
+		for (let k in o) {
+			if (new RegExp('(' + k + ')').test(fmt)) {
+				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+			}
+		}
+		return fmt;
+	} else {
+		//TODO
+		value = value.trim();
+		return value.substr(0, fmt.length);
+	}
+}
+
+/**
+ * 价格格式化
+ * @param value
+ * @param fmt
+ * @returns {*}
+ */
+export function formatPrice(value) {
+	var result = Math.round(parseFloat(value) * 100) / 100
+	const arrayNum = value.toString().split('.')
+	if (arrayNum.length < 2) {
+		return value.toString() + '.00'
+	} else {
+		if (arrayNum[1].length < 2) {
+			return value.toString() + '0'
+		} else {
+			return value.toString()
+		}
+	}
+}
