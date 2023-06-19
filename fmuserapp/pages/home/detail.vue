6ee2cb598c23b6f2b27747c57e6aaf0c2c103573
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-20 06:05:17 +0800

    Update detail.vue

--- fmuserapp/pages/home/detail.vue
+++ fmuserapp/pages/home/detail.vue
@@ -2,17 +2,26 @@
 	<view>
 		<view class="combo-info" v-if="data">
 			<image :src="data.image.url" mode="aspectFill" @click="onPreview(data.image.url)"></image>
+			<view class="info-box">
+				<text>{{data.name}}</text>
+				<text>￥{{data.sell_price}}</text>
+			</view>
 		</view>
-		<view class="combo-sku">
-
+		<view class="divider"></view>
+		<view class="combo-sku" v-if="data">
+			<text>产品内容</text>
+			<view class="sku-list" v-for="(item, index) in data.sku" :key="index">
+				{{item}}
+			</view>
 		</view>
-		<view class="combo-comment">
-
+		<view class="divider"></view>
+		<view class="combo-comment" v-if="data">
+			<text>用户评价</text>
 		</view>
-		<view class="combo-detail">
-
+		<view class="divider"></view>
+		<view class="combo-detail" v-if="data">
+			<text>{{data.description}}</text>
 		</view>
-		<!-- {{data}} -->
 	</view>
 </template>
 
@@ -48,16 +57,42 @@
 </script>
 
 <style lang="scss" scoped>
+	.divider {
+		height: 16rpx;
+		background-color: #f2f2f2;
+	}
+
 	.combo-info {
 		image {
 			width: 100%;
 			height: 400rpx;
 		}
+
+		.info-box {
+			background-color: #fff;
+			padding-left: 40rpx;
+			padding-top: 16rpx;
+			padding-bottom: 16rpx;
+			display: flex;
+			flex-direction: column;
+
+			text:nth-last-child(1) {
+				color: red;
+			}
+		}
 	}
 
-	.combo-sku {}
+	.combo-sku {
+		padding: 40rpx;
+	}
 
-	.combo-comment {}
+	.combo-comment {
+		padding: 40rpx;
+	}
 
-	.combo-detail {}
+	.combo-detail {
+		padding: 40rpx;
+
+		text {}
+	}
 </style>
