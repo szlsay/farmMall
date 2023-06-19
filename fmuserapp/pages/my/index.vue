5a39a6ecae8872687ae8d504d0033cac0c255876
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-20 07:33:20 +0800

    Update index.vue

--- fmuserapp/pages/my/index.vue
+++ fmuserapp/pages/my/index.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		我的
+		<view class="my-bg"></view>
+		<view class="my-info">未登录</view>
+		<view class="my-menu"></view>
 	</view>
 </template>
 
@@ -17,6 +19,16 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.my-bg {}
 
+	.my-info {
+		margin: 40rpx;
+		background: #fff;
+		box-shadow: 0px 3px 6px 0px rgba(173, 174, 179, 0.09), 0px -3px 6px 0px rgba(173, 174, 179, 0.09);
+		border-radius: 20rpx;
+		padding: 32rpx;
+		margin-bottom: 40rpx;
+		display: flex;
+	}
 </style>
