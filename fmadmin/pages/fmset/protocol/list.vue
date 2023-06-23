d941a2b2749b7a94995da701fd22b69f924d0666
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-23 16:20:03 +0800

    Update list.vue

--- fmadmin/pages/fmset/protocol/list.vue
+++ fmadmin/pages/fmset/protocol/list.vue
@@ -6,29 +6,20 @@
 				<view class="uni-sub-title"></view>
 			</view>
 			<view class="uni-group">
-				<input class="uni-search" type="text" v-model="query" @confirm="search" placeholder="请输入搜索内容" />
-				<button class="uni-button" type="default" size="mini" @click="search">搜索</button>
 				<button class="uni-button" type="default" size="mini" @click="navigateTo('./add')">新增</button>
-				<button class="uni-button" type="default" size="mini" :disabled="!selectedIndexs.length"
-					@click="delTable">批量删除</button>
 			</view>
 		</view>
 		<view class="uni-container">
-			<unicloud-db ref="udb" :collection="collectionList" field="create_time,update_time,title,type" :where="where"
-				page-data="replace" :orderby="orderby" :getcount="true" :page-size="options.pageSize"
-				:page-current="options.pageCurrent" v-slot:default="{data,pagination,loading,error,options}" :options="options"
-				loadtime="manual">
-				<uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe type="selection"
-					@selection-change="selectionChange">
+			<unicloud-db ref="udb" :collection="collectionList" field="create_time,update_time,title,type"
+				:where="where" page-data="replace" :orderby="orderby" :getcount="true" :page-size="options.pageSize"
+				:page-current="options.pageCurrent" v-slot:default="{data,pagination,loading,error,options}"
+				:options="options" loadtime="manual">
+				<uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe>
 					<uni-tr>
-						<uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'title')" sortable
-							@sort-change="sortChange($event, 'title')">协议标题</uni-th>
-						<uni-th align="center" filter-type="select" :filter-data="options.filterData.type_localdata"
-							@filter-change="filterChange($event, 'type')">协议类型</uni-th>
-						<uni-th align="center" filter-type="timestamp" @filter-change="filterChange($event, 'create_time')" sortable
-							@sort-change="sortChange($event, 'create_time')">创建时间</uni-th>
-						<uni-th align="center" filter-type="timestamp" @filter-change="filterChange($event, 'update_time')" sortable
-							@sort-change="sortChange($event, 'update_time')">更新时间</uni-th>
+						<uni-th align="center">协议标题</uni-th>
+						<uni-th align="center">协议类型</uni-th>
+						<uni-th align="center">创建时间</uni-th>
+						<uni-th align="center">更新时间</uni-th>
 						<uni-th align="center">操作</uni-th>
 					</uni-tr>
 					<uni-tr v-for="(item,index) in data" :key="index">
@@ -44,14 +35,15 @@
 							<view class="uni-group">
 								<button @click="navigateTo('./edit?id='+item._id, false)" class="uni-button" size="mini"
 									type="primary">修改</button>
-								<button @click="confirmDelete(item._id)" class="uni-button" size="mini" type="warn">删除</button>
+								<button @click="confirmDelete(item._id)" class="uni-button" size="mini"
+									type="warn">删除</button>
 							</view>
 						</uni-td>
 					</uni-tr>
 				</uni-table>
 				<view class="uni-pagination-box">
-					<uni-pagination show-icon :page-size="pagination.size" v-model="pagination.current" :total="pagination.count"
-						@change="onPageChanged" />
+					<uni-pagination show-icon :page-size="pagination.size" v-model="pagination.current"
+						:total="pagination.count" @change="onPageChanged" />
 				</view>
 			</unicloud-db>
 		</view>
@@ -157,60 +149,13 @@
 					}
 				})
 			},
-			// 多选处理
-			selectedItems() {
-				var dataList = this.$refs.udb.dataList
-				return this.selectedIndexs.map(i => dataList[i]._id)
-			},
-			// 批量删除
-			delTable() {
-				this.$refs.udb.remove(this.selectedItems(), {
-					success: (res) => {
-						this.$refs.table.clearSelection()
-					}
-				})
-			},
-			// 多选
-			selectionChange(e) {
-				this.selectedIndexs = e.detail.index
-			},
 			confirmDelete(id) {
 				this.$refs.udb.remove(id, {
 					success: (res) => {
 						this.$refs.table.clearSelection()
 					}
 				})
-			},
-			sortChange(e, name) {
-				this.orderByFieldName = name;
-				if (e.order) {
-					this.orderby = name + ' ' + orderByMapping[e.order]
-				} else {
-					this.orderby = ''
-				}
-				this.$refs.table.clearSelection()
-				this.$nextTick(() => {
-					this.$refs.udb.loadData()
-				})
-			},
-			filterChange(e, name) {
-				this._filter[name] = {
-					type: e.filterType,
-					value: e.filter
-				}
-				let newWhere = filterToWhere(this._filter, db.command)
-				if (Object.keys(newWhere).length) {
-					this.where = newWhere
-				} else {
-					this.where = ''
-				}
-				this.$nextTick(() => {
-					this.$refs.udb.loadData()
-				})
 			}
 		}
 	}
 </script>
-
-<style>
-</style>
