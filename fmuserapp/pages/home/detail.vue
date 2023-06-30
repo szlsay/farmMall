579acac2413bfc7d4d58616bf1a8ea7131bd5ff0
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-30 12:13:23 +0800

    Update detail.vue

--- fmuserapp/pages/home/detail.vue
+++ fmuserapp/pages/home/detail.vue
@@ -10,18 +10,22 @@
 		<view class="box combo-sku" v-if="data">
 			<text class="combo-title">产品内容</text>
 			<view class="sku-list" v-for="(item, index) in data.sku" :key="index">
-				<image :src="item.image.url" mode="aspectFill"></image>
-				{{item.product_name}}
-				{{item.unit}}
-				{{item.qty}}
+				<view class="left-list">
+					<image :src="item.image.url" mode="aspectFill"></image>
+					<view class="sku-info">
+						<text class="combo-title"> {{item.product_name}}</text>
+						<text> {{item.qty}}{{getMeasureUnitText(item.unit)}}</text>
+					</view>
+				</view>
+				<view class="right-list">
+					<uni-icons type="right" size="24"></uni-icons>
+				</view>
 			</view>
 		</view>
-		<view class="divider"></view>
-		<view class="combo-comment" v-if="data">
+		<view class="box combo-comment" v-if="data">
 			<text class="combo-title">用户评价</text>
 		</view>
-		<view class="divider"></view>
-		<view class="combo-detail" v-if="data">
+		<view class="box combo-detail" v-if="data">
 			<text>{{data.description}}</text>
 		</view>
 	</view>
@@ -40,6 +44,14 @@
 			this.getData()
 		},
 		methods: {
+			getMeasureUnitText(value) {
+				const result = this.$store.state.sys.measure_units.filter(item => item.value === value)
+				if (result && result.length > 0) {
+					return result[0].text
+				} else {
+					return ''
+				}
+			},
 			onPreview(url) {
 				uni.previewImage({
 					urls: [url],
@@ -80,13 +92,39 @@
 			}
 		}
 	}
-	
-	.combo-title{
+
+	.combo-title {
 		font-weight: bold;
 	}
 
 	.combo-sku {
-		
+		.sku-list {
+			display: flex;
+			justify-content: space-between;
+			margin-top: 32rpx;
+
+			.left-list {
+				display: flex;
+
+				image {
+					width: 200rpx;
+					height: 150rpx;
+					border-radius: 10rpx;
+				}
+				.sku-info{
+					padding-left: 20rpx;
+					
+					display: flex;
+					flex-direction: column;
+					justify-content:space-around;
+				}
+			}
+			.right-list{
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		}
 	}
 
 	.combo-comment {
