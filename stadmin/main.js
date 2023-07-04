410a9a19549ccecd9df4ad29e25c5aca4c96d16a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-04 12:28:40 +0800

    main

--- stadmin/main.js
+++ stadmin/main.js
@@ -3,11 +3,9 @@ import store from './store'
 import plugin from './js_sdk/uni-admin/plugin'
 import messages from './i18n/index.js'
 const lang = uni.getLocale()
+
 import { createSSRApp } from 'vue'
 import { createI18n } from 'vue-i18n'
-
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 export function createApp() {
   const app = createSSRApp(App)
@@ -18,8 +16,8 @@ export function createApp() {
   app.use(i18n)
   app.use(plugin)
   app.use(store)
-	app.use(ElementPlus)
   return {
     app
   }
 }
+
