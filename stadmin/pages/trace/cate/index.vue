cb8558e0a3086c21ae1a4ca8dd9d708c41ac7671
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-04 11:02:41 +0800

    cateindex

---
+++ stadmin/pages/trace/cate/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		{{content}}
+	</view>
+</template>
+<script setup>
+	import {
+		ref
+	} from "vue";
+	const content = ref(123)
+</script>
+<style lang="scss" scoped>
+
+</style>
