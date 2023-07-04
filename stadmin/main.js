de2817f59abd116bd15e0b057a475f0e375333f9
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-04 09:49:04 +0800

    elementui

--- stadmin/main.js
+++ stadmin/main.js
@@ -2,10 +2,13 @@ import App from './App'
 import store from './store'
 import plugin from './js_sdk/uni-admin/plugin'
 import messages from './i18n/index.js'
-
 const lang = uni.getLocale()
 import { createSSRApp } from 'vue'
 import { createI18n } from 'vue-i18n'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 export function createApp() {
   const app = createSSRApp(App)
   const i18n = createI18n({
@@ -15,6 +18,7 @@ export function createApp() {
   app.use(i18n)
   app.use(plugin)
   app.use(store)
+	app.use(ElementPlus)
   return {
     app
   }
