fec5333c70de61e6aa8f3c2f48bc102042352f33
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-05 10:08:31 +0800

    coop

---
+++ stadmin/js_sdk/validator/st-coop.js
@@ -0,0 +1,136 @@
+// 表单校验规则由 schema2code 生成，不建议直接修改校验规则，而建议通过 schema2code 生成, 详情: https://uniapp.dcloud.net.cn/uniCloud/schema
+
+
+const validator = {
+  "coop_name": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      }
+    ],
+    "label": "合作社名称"
+  },
+  "contact_name": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "label": "联系人姓名"
+  },
+  "contact_phone": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "pattern": "^1[34578][0-9]{9}$"
+      }
+    ],
+    "label": "联系人电话"
+  },
+  "address": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "label": "详细地址"
+  },
+  "longitude": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "label": "经度"
+  },
+  "latitude": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "label": "纬度"
+  },
+  "scope": {
+    "rules": [
+      {
+        "format": "string"
+      },
+      {
+        "maxLength": 200
+      }
+    ],
+    "label": "主营范围"
+  },
+  "image": {
+    "rules": [
+      {
+        "format": "file"
+      }
+    ],
+    "label": "合作社图片"
+  },
+  "disabled": {
+    "rules": [
+      {
+        "format": "bool"
+      }
+    ],
+    "label": "是否禁用",
+    "defaultValue": "false"
+  }
+}
+
+const enumConverter = {}
+
+function filterToWhere(filter, command) {
+  let where = {}
+  for (let field in filter) {
+    let { type, value } = filter[field]
+    switch (type) {
+      case "search":
+        if (typeof value === 'string' && value.length) {
+          where[field] = new RegExp(value)
+        }
+        break;
+      case "select":
+        if (value.length) {
+          let selectValue = []
+          for (let s of value) {
+            selectValue.push(command.eq(s))
+          }
+          where[field] = command.or(selectValue)
+        }
+        break;
+      case "range":
+        if (value.length) {
+          let gt = value[0]
+          let lt = value[1]
+          where[field] = command.and([command.gte(gt), command.lte(lt)])
+        }
+        break;
+      case "date":
+        if (value.length) {
+          let [s, e] = value
+          let startDate = new Date(s)
+          let endDate = new Date(e)
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+      case "timestamp":
+        if (value.length) {
+          let [startDate, endDate] = value
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+    }
+  }
+  return where
+}
+
+export { validator, enumConverter, filterToWhere }
---
+++ stadmin/pages/trace/coop/edit.vue
@@ -0,0 +1,152 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
+      <uni-forms-item name="coop_name" label="合作社名称" required>
+        <uni-easyinput placeholder="请输入合作社名称" v-model="formData.coop_name" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="contact_name" label="联系人姓名">
+        <uni-easyinput placeholder="请输入联系人姓名" v-model="formData.contact_name" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="contact_phone" label="联系人电话">
+        <uni-easyinput placeholder="请输入联系人电话" v-model="formData.contact_phone" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="address" label="详细地址">
+        <uni-easyinput placeholder="请填写详细地址" v-model="formData.address" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="longitude" label="经度">
+        <uni-easyinput placeholder="请选择经度" v-model="formData.longitude" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="latitude" label="纬度">
+        <uni-easyinput placeholder="请选择纬度" v-model="formData.latitude" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="scope" label="主营范围">
+        <uni-easyinput placeholder="请填写主营范围" v-model="formData.scope" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="image" label="合作社图片">
+        <uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="object" v-model="formData.image"></uni-file-picker>
+      </uni-forms-item>
+      <uni-forms-item name="disabled" label="是否禁用">
+        <switch @change="binddata('disabled', $event.detail.value)" :checked="formData.disabled"></switch>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '@/js_sdk/validator/st-coop.js';
+
+  const db = uniCloud.database();
+  const dbCmd = db.command;
+  const dbCollectionName = 'st-coop';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.includes(key)) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "coop_name": "",
+        "contact_name": "",
+        "contact_phone": "",
+        "address": "",
+        "longitude": "",
+        "latitude": "",
+        "scope": "",
+        "image": null,
+        "disabled": "false"
+      }
+      return {
+        formData,
+        formOptions: {},
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onLoad(e) {
+      if (e.id) {
+        const id = e.id
+        this.formDataId = id
+        this.getDetail(id)
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).doc(this.formDataId).update(value).then((res) => {
+          uni.showToast({
+            title: '修改成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      },
+
+      /**
+       * 获取表单数据
+       * @param {Object} id
+       */
+      getDetail(id) {
+        uni.showLoading({
+          mask: true
+        })
+        db.collection(dbCollectionName).doc(id).field("coop_name,contact_name,contact_phone,address,longitude,latitude,scope,image,disabled").get().then((res) => {
+          const data = res.result.data[0]
+          if (data) {
+            this.formData = data
+            
+          }
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      }
+    }
+  }
+</script>
