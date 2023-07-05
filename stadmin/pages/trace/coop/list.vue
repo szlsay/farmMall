5a466ab1eb14a06517248f93b8c73259738a1dab
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-06 03:04:00 +0800

    Update list.vue

--- stadmin/pages/trace/coop/list.vue
+++ stadmin/pages/trace/coop/list.vue
@@ -9,7 +9,6 @@
 				<input class="uni-search" type="text" v-model="query" @confirm="search" placeholder="请输入搜索内容" />
 				<button class="uni-button" type="default" size="mini" @click="search">搜索</button>
 				<button class="uni-button" type="default" size="mini" @click="navigateTo('./add')">新增</button>
-				<!--        <button class="uni-button" type="default" size="mini" :disabled="!selectedIndexs.length" @click="delTable">批量删除</button> -->
 				<download-excel class="hide-on-phone" :fields="exportExcel.fields" :data="exportExcelData"
 					:type="exportExcel.type" :name="exportExcel.filename">
 					<button class="uni-button" type="primary" size="mini">导出 Excel</button>
@@ -20,12 +19,12 @@
 			<unicloud-db ref="udb" :collection="collectionList"
 				field="coop_name,contact_name,contact_phone,map_address,address,scope,image,disabled" :where="where"
 				page-data="replace" :orderby="orderby" :getcount="true" :page-size="options.pageSize"
-				:page-current="options.pageCurrent" v-slot:default="{data,pagination,loading,error,options}" :options="options"
-				loadtime="manual" @load="onqueryload">
-				<uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe type="selection"
-					@selection-change="selectionChange">
+				:page-current="options.pageCurrent" v-slot:default="{data,pagination,loading,error,options}"
+				:options="options" loadtime="manual" @load="onqueryload">
+				<uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe>
 					<uni-tr>
-						<uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'coop_name')" sortable
+						<uni-th width="120" align="center" filter-type="search"
+							@filter-change="filterChange($event, 'coop_name')" sortable
 							@sort-change="sortChange($event, 'coop_name')">合作社名称</uni-th>
 						<uni-th width="120" align="center" filter-type="search"
 							@filter-change="filterChange($event, 'contact_name')" sortable
@@ -36,7 +35,8 @@
 						<uni-th align="center">详细地址</uni-th>
 						<uni-th align="center">主营范围</uni-th>
 						<uni-th align="center">合作社图片</uni-th>
-						<uni-th width="100" align="center" sortable @sort-change="sortChange($event, 'disabled')">是否禁用</uni-th>
+						<uni-th width="100" align="center" sortable
+							@sort-change="sortChange($event, 'disabled')">是否禁用</uni-th>
 						<uni-th align="center">操作</uni-th>
 					</uni-tr>
 					<uni-tr v-for="(item,index) in data" :key="index">
@@ -54,14 +54,15 @@
 							<view class="uni-group">
 								<button @click="navigateTo('./edit?id='+item._id, false)" class="uni-button" size="mini"
 									type="primary">修改</button>
-								<button @click="confirmDelete(item._id)" class="uni-button" size="mini" type="warn">删除</button>
+								<button @click="confirmDelete(item._id)" class="uni-button" size="mini"
+									type="warn">删除</button>
 							</view>
 						</uni-td>
 					</uni-tr>
 				</uni-table>
 				<view class="uni-pagination-box">
-					<uni-pagination show-icon :page-size="pagination.size" v-model="pagination.current" :total="pagination.count"
-						@change="onPageChanged" />
+					<uni-pagination show-icon :page-size="pagination.size" v-model="pagination.current"
+						:total="pagination.count" @change="onPageChanged" />
 				</view>
 			</unicloud-db>
 		</view>
@@ -182,10 +183,6 @@
 						this.$refs.table.clearSelection()
 					}
 				})
-			},
-			// 多选
-			selectionChange(e) {
-				this.selectedIndexs = e.detail.index
 			},
 			confirmDelete(id) {
 				this.$refs.udb.remove(id, {
