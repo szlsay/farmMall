bb489f50b8777814589c184618969d25b8f63580
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-06 12:56:37 +0800

    Update list.vue

--- stadmin/pages/trace/basic/list.vue
+++ stadmin/pages/trace/basic/list.vue
@@ -6,30 +6,22 @@
         <view class="uni-sub-title"></view>
       </view>
       <view class="uni-group">
-        <input class="uni-search" type="text" v-model="query" @confirm="search" placeholder="请输入搜索内容" />
-        <button class="uni-button" type="default" size="mini" @click="search">搜索</button>
         <button class="uni-button" type="default" size="mini" @click="navigateTo('./add')">新增</button>
-        <button class="uni-button" type="default" size="mini" :disabled="!selectedIndexs.length" @click="delTable">批量删除</button>
-        <download-excel class="hide-on-phone" :fields="exportExcel.fields" :data="exportExcelData" :type="exportExcel.type" :name="exportExcel.filename">
-          <button class="uni-button" type="primary" size="mini">导出 Excel</button>
-        </download-excel>
       </view>
     </view>
     <view class="uni-container">
       <unicloud-db ref="udb" :collection="collectionList" field="text,value,list" :where="where" page-data="replace"
         :orderby="orderby" :getcount="true" :page-size="options.pageSize" :page-current="options.pageCurrent"
         v-slot:default="{data,pagination,loading,error,options}" :options="options" loadtime="manual" @load="onqueryload">
-        <uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe type="selection" @selection-change="selectionChange">
+        <uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe>
           <uni-tr>
-            <uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'text')" sortable @sort-change="sortChange($event, 'text')">标题</uni-th>
-            <uni-th align="center" filter-type="search" @filter-change="filterChange($event, 'value')" sortable @sort-change="sortChange($event, 'value')">标题拼音</uni-th>
-            <uni-th align="center" sortable @sort-change="sortChange($event, 'list')">列表</uni-th>
+            <uni-th align="center">标题</uni-th>
+            <uni-th align="center">标题拼音</uni-th>
             <uni-th align="center">操作</uni-th>
           </uni-tr>
           <uni-tr v-for="(item,index) in data" :key="index">
             <uni-td align="center">{{item.text}}</uni-td>
             <uni-td align="center">{{item.value}}</uni-td>
-            <uni-td align="center">{{item.list}}</uni-td>
             <uni-td align="center">
               <view class="uni-group">
                 <button @click="navigateTo('./edit?id='+item._id, false)" class="uni-button" size="mini" type="primary">修改</button>
