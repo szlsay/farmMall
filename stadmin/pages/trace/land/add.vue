c65cd7769f93ede2250e0b720d6860d9c65afe2f
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-07 09:18:35 +0800

    Update add.vue

--- stadmin/pages/trace/land/add.vue
+++ stadmin/pages/trace/land/add.vue
@@ -6,15 +6,13 @@
 				<view class="st-card-header">基本信息</view>
 				<uni-row>
 					<uni-col :xs="24" :sm="12">
-						<uni-forms-item name="land_name" label="地块名称" required :label-width="labelWidth"
-							label-align="right">
+						<uni-forms-item name="land_name" label="地块名称" required :label-width="labelWidth" label-align="right">
 							<uni-easyinput placeholder="例如：一号地块、一号大棚" v-model="formData.land_name" trim="both"
 								maxlength="20"></uni-easyinput>
 						</uni-forms-item>
 					</uni-col>
 					<uni-col :xs="24" :sm="12">
-						<uni-forms-item name="land_type" label="地块类型" required :label-width="labelWidth"
-							label-align="right">
+						<uni-forms-item name="land_type" label="地块类型" required :label-width="labelWidth" label-align="right">
 							<uni-data-select placeholder="请选择地块类型" v-model="formData.land_type"
 								:localdata="$store.state.sys.land_types"></uni-data-select>
 						</uni-forms-item>
@@ -28,8 +26,7 @@
 						</uni-forms-item>
 					</uni-col>
 					<uni-col :xs="24" :sm="12">
-						<uni-forms-item name="contact_phone" label="负责人电话" :label-width="labelWidth"
-							label-align="right">
+						<uni-forms-item name="contact_phone" label="负责人电话" :label-width="labelWidth" label-align="right">
 							<uni-easyinput placeholder="请输入负责人电话" v-model="formData.contact_phone" trim="both"
 								maxlength="11"></uni-easyinput>
 						</uni-forms-item>
@@ -39,14 +36,12 @@
 					<uni-forms-item name="image_content" label="地块实景" :label-width="labelWidth" label-align="right">
 						<uni-file-picker file-mediatype="image" file-extname="jpg,png,webp" return-type="array"
 							v-model="formData.image_content" limit="6" :image-styles="imageStyles"></uni-file-picker>
-						<text style="color: red; font-size: 14px;"
-							class="title-alert">用于地块的详情展示，最多六张。格式：jpg,png,webp</text>
+						<text style="color: red; font-size: 14px;" class="title-alert">用于地块的详情展示，最多六张。格式：jpg,png,webp</text>
 					</uni-forms-item>
 				</uni-row>
 				<uni-row>
 					<uni-forms-item name="video" label="实景视频" :label-width="labelWidth" label-align="right">
-						<uni-file-picker file-mediatype="file" return-type="object"
-							v-model="formData.video"></uni-file-picker>
+						<uni-file-picker file-mediatype="file" return-type="object" v-model="formData.video"></uni-file-picker>
 					</uni-forms-item>
 				</uni-row>
 				<uni-row>
@@ -58,8 +53,8 @@
 				<uni-row>
 					<uni-col :xs="24" :sm="8">
 						<uni-forms-item name="map_address" label="地图选址" :label-width="labelWidth" label-align="right">
-							<uni-easyinput type="textarea" placeholder="请从地图上选址" v-model="formData.map_address"
-								trim="both" disabled></uni-easyinput>
+							<uni-easyinput type="textarea" placeholder="请从地图上选址" v-model="formData.map_address" trim="both"
+								disabled></uni-easyinput>
 						</uni-forms-item>
 					</uni-col>
 					<uni-col :xs="24" :sm="8">
@@ -70,12 +65,10 @@
 					</uni-col>
 					<uni-col :xs="24" :sm="8">
 						<uni-forms-item name="longitude" label="经度" :label-width="labelWidth" label-align="right">
-							<uni-easyinput placeholder="请选择经度" v-model="formData.longitude" trim="both"
-								disabled></uni-easyinput>
+							<uni-easyinput placeholder="请选择经度" v-model="formData.longitude" trim="both" disabled></uni-easyinput>
 						</uni-forms-item>
 						<uni-forms-item name="latitude" label="纬度" :label-width="labelWidth" label-align="right">
-							<uni-easyinput placeholder="请选择纬度" v-model="formData.latitude" trim="both"
-								disabled></uni-easyinput>
+							<uni-easyinput placeholder="请选择纬度" v-model="formData.latitude" trim="both" disabled></uni-easyinput>
 						</uni-forms-item>
 					</uni-col>
 				</uni-row>
@@ -87,8 +80,7 @@
 				<uni-row :gutter="20">
 					<uni-col :xs="24" :sm="6">
 						<view class="map-search">
-							<input class="uni-search" type="text" v-model="queryMap" @confirm="onSearch"
-								placeholder="请输入搜索地址" />
+							<input class="uni-search" type="text" v-model="queryMap" @confirm="onSearch" placeholder="请输入搜索地址" />
 							<button class="uni-button" type="default" size="mini" @click="onSearch">搜索</button>
 						</view>
 						<view class="map-list" v-for="item in searchList" @click="onClickMap(item)">
@@ -255,8 +247,11 @@
 						satellite,
 						roadNet
 					],
+					mapStyle: "amap://styles/darkblue",
+					features: ['bg', 'road', 'building', 'point'],
 					center //初始化地图中心点位置
 				});
+				// this.map.setMapStyle("amap://styles/dark");
 				// 地图点击事件--点标记标注
 				this.map.on("click", this.handleClick);
 
@@ -349,7 +344,7 @@
 				this.marker.setMap(this.map);
 				// 设置label标签，label默认蓝框白底左上角显示，样式className为：amap-marker-label
 				const content =
-					"<div style='height: 20px; line-height: 20px; border-radius:10px; padding-left: 8px; padding-right: 8px; font-size: 16px; font-weight: 700; color: #00CC99; border: 1px solid #00CC99; text-shadow: 1px 1px 2px #eee;'>" +
+					"<div style='height: 20px; line-height: 20px; border-radius:14px; padding-left: 12px; padding-right: 12px; font-size: 16px; font-weight: 700; color: white; border: 2px solid #00CC99; text-shadow: 1px 1px 2px #000;'>" +
 					address + "</div>"
 				this.marker.setLabel({
 					direction: "top-center",
@@ -398,7 +393,7 @@
 	#map {
 		width: 100%;
 		height: 400px;
-		border-radius: 20rpx;
+		border-radius: 8rpx;
 		position: relative;
 	}
 
