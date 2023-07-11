e3e987b384a2dd7422447aa9b29bbc3973f5b656
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-11 11:33:16 +0800

    Update st-farmer.schema.ext.js

--- stadmin/uniCloud-aliyun/database/st-farmer.schema.ext.js
+++ stadmin/uniCloud-aliyun/database/st-farmer.schema.ext.js
@@ -9,17 +9,7 @@ module.exports = {
 		},
 		beforeUpdate: async function(obj = {}) {
 			console.log('farm-beforeUpdate', obj)
-			// let {
-			// 	collection,
-			// 	operation,
-			// 	where,
-			// 	field
-			// } = obj;
-			// const db = uniCloud.database();
-			// const dataUpdata = {
-			// 	update_time: Date.now()
-			// }
-			// await uniCloud.database().collection(collection).where(where).update(dataUpdata)
+			obj.updateData.update_time = Date.now()
 		},
 		afterUpdate: async function(obj = {}) {
 			console.log('farm-afterUpdate', obj)
