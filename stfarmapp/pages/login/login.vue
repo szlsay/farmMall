c9cb683125d2ef379dbed0d59b361418a714af51
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-12 15:03:13 +0800

    login

--- stfarmapp/pages/login/login.vue
+++ stfarmapp/pages/login/login.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		login
+		<view class="bg">
+			<image src="@/static/login-bg.webp" mode="heightFix" class="move-animation"></image>
+		</view>
+		<view class="login-container">
+		</view>
 	</view>
 </template>
 
@@ -17,6 +21,33 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.bg {
+		height: 100vh;
+		position: relative;
 
+		image {
+			height: 100%;
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+
+		.move-animation {
+			animation-name: move;
+			animation-duration: 30s;
+			animation-timing-function: linear;
+			animation-iteration-count: infinite;
+		}
+
+		@keyframes move {
+			from {
+				transform: translateX(0);
+			}
+
+			to {
+				transform: translateX(50%);
+			}
+		}
+	}
 </style>
