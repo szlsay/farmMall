d563b159802e4d72b8d903010c19bad89eb897ab
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-13 12:14:41 +0800

    login

---
+++ stfarmapp/components/st-loginbg/st-loginbg.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="login-bg">
+		<image src="https://mp-e548ea67-cedb-4a2f-b6fe-005d83c724b8.cdn.bspapp.com/images/login-bg.webp" mode="heightFix"
+			class="move-animation"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "st-loginbg",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.login-bg {
+		image {
+			height: 100%;
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+
+		.move-animation {
+			animation-name: move;
+			animation-delay: 100ms;
+			animation-duration: 30s;
+			animation-timing-function: linear;
+			animation-iteration-count: infinite;
+			animation-direction: alternate;
+		}
+
+		@keyframes move {
+			from {
+				transform: translateX(0%);
+			}
+
+			to {
+				transform: translateX(50%);
+			}
+		}
+	}
+</style>
