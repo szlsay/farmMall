c9ac1f2e81368a818e5ad6cd1905abac960fe41b
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-13 15:48:11 +0800

    home

--- stfarmapp/pages/home/home.vue
+++ stfarmapp/pages/home/home.vue
@@ -1,18 +1,18 @@
 <template>
 	<view>
-		
+		<view class="">
+			请登录
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {
-				
-			}
+			return {}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
