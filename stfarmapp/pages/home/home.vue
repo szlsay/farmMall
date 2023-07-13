1be2b13dd2ed7a1b366e4941b8fb1142cc73bf59
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-13 21:00:14 +0800

    Update home.vue

--- stfarmapp/pages/home/home.vue
+++ stfarmapp/pages/home/home.vue
@@ -5,8 +5,20 @@
 		</view>
 		<view class="work-container">
 			<text class="title">信息操作</text>
-			<view class="st-box" v-for="i in 4">
-				<image src="../../static/home/home-map.png" mode=""></image>
+			<view class="st-box" v-for="(item, index) in menus" :key="index">
+				<view class="left-item">
+					<view class="left-up">
+						<image :src="item.img" mode="" class="icon"></image>
+						<text :style="{color: item.color}" class="text">{{item.text}}</text>
+					</view>
+					<view class="">
+						<text :style="{color: item.color}" class="num">{{item.num}}</text>
+						<text :style="{color: item.color}" class="type">个</text>
+					</view>
+				</view>
+				<view class="right-item">
+					<image :src="item.img" mode=""></image>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -15,7 +27,15 @@
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+				menus: [{
+					"img": "/static/home/home-map.png",
+					"text": "圈地",
+					"color": "#2294da",
+					"num": "5",
+					"to": ""
+				}]
+			}
 		},
 		methods: {
 
@@ -38,10 +58,53 @@
 	.st-box {
 		border-radius: 8rpx;
 		box-shadow: 2rpx 2rpx 10rpx 0 rgba(0, 0, 0, 0.1);
+		margin-top: 40rpx;
 		margin-bottom: 40rpx;
-		height: 150rpx;
-		position: relative;
-		overflow: hidden;
+		height: 200rpx;
+
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.left-item {
+		height: 100%;
+		padding-left: 40rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+
+		.left-up {
+			display: flex;
+			justify-content: center;
+
+			.icon {
+				width: 48rpx;
+				height: 48rpx;
+			}
+
+			.text {
+				font-size: 36rpx;
+				font-weight: bold;
+				margin-left: 10rpx;
+			}
+
+		}
+
+		.num {
+			font-size: 64rpx;
+			font-weight: bold;
+		}
+
+		.type {
+			margin-left: 6rpx;
+			font-size: 30rpx;
+		}
+	}
+
+	.right-item {
+		height: 100%;
+		// position: relative;
+		// overflow: hidden;
 
 		image {
 			position: absolute;
@@ -52,6 +115,8 @@
 			height: 128rpx;
 		}
 	}
+
+
 
 	.login-container {
 		padding: 40rpx 40rpx 0rpx;
